3c2d3c220c52695a25348a42294b4f43b9fc042f
Yashesh Bharti <bhartiyashesh@gmail.com>
2021-02-26 15:30:49 -0600

    Added Login and Register pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ApplyComponent } from './apply/apply.component';
+import { HomeComponent } from './home/home.component';
+import {LoginComponent} from './login/login.component'
+import { RegisterComponent } from './register/register.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'apply', component: ApplyComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
